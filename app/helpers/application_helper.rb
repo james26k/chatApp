49d7579d6767713119c98f82168e75f1fb5248ff
james26k <cb26.hayashi@gmail.com>
2020-08-22 14:35:05 +0900

    application_helperにタイトルを返す関数を定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページ毎に完全なタイトルを返す。.html.erbから下記の関数を呼び出すことでhtmlに文字列を返す
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
