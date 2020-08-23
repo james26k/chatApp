cf086a32c91c931cc40b01ce21195d7a194ee251
james26k <cb26.hayashi@gmail.com>
2020-08-23 12:27:21 +0900

    nameをfirst_nameとlast_nameに分割

--- example_user.rb
+++ example_user.rb
@@ -1,14 +1,15 @@
 class User
   #UserとEmailに対応するaccessorを作成する
   #Getter, Setterをそれぞれ定義してくれる
-  attr_accessor :name, :email
+  attr_accessor :first_name, :last_name, :email
 
   def initialize(attributes = {})
-    @name = attributes[:name]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
     @email = attributes[:email]
   end
 
   def formatted_email
-    "#{@name} <#{@email}>"
+    "#{@first_name} #{@last_name} <#{@email}>"
   end
 end
